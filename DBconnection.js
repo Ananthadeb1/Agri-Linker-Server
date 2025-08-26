8441164720b3679c5d71ad17d34f81d87344fb8c
Anantha Debnath <ananthadebnath103@gmail.com>
2025-08-26 13:49:59 +0600

    successfully get data from the database

--- DBconnection.js
+++ DBconnection.js
@@ -1,4 +1,3 @@
-// DBconnection.js
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emv2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -11,16 +10,17 @@ const client = new MongoClient(uri, {
   },
 });
 
-// connect function
 async function connectDB() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("✅ MongoDB Connected Successfully!");
-    return client; // return client so you can use it in routes
+    console.log(
+      "✅ Pinged your deployment. Successfully connected to MongoDB!"
+    );
+    return client; // return the connected client
   } catch (error) {
     console.error("❌ Failed to connect to MongoDB:", error);
-    process.exit(1); // stop server if DB connection fails
+    process.exit(1);
   }
 }
 
